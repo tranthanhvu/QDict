d2371a18a16b8e55bfb4c9e3ae926cc386ed66b1
Trần Thanh Vũ <ttvu90@gmail.com>
2020-07-06 22:21:03 +0700

    first submit

---
+++ QDict/Shared/QDictApp.swift
@@ -0,0 +1,17 @@
+//
+//  QDictApp.swift
+//  Shared
+//
+//  Created by Yoyo on 7/6/20.
+//
+
+import SwiftUI
+
+@main
+struct QDictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
